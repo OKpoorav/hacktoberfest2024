9000022962f6065e3e560b11ce396f7ea087167f
okpoorav <hipoorav@gmail.com>
2024-10-02 15:56:16 +0530

    add new file java

---
+++ scripts/helloworldbypoorav.java
@@ -0,0 +1,8 @@
+// Author: Poorav Kumar Gupta
+// GitHub: https://github.com/OKpoorav
+// Language: Java
+public class HelloWorldJava {
+  public static void main() {
+    System.out.println("Hello world!");
+  }
+}
